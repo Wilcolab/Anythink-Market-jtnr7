6905dc4c797a28348923fe9aba5a07726cd9358e
David <david@c940>
2022-08-12 16:39:20 +0200

    fix api url env

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -6,7 +6,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 const API_ROOT =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : "https://anythink-market-jtnr7-api.herokuapp.com/api/ping";
+    : process.env.REACT_APP_BACKEND_URL;
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
