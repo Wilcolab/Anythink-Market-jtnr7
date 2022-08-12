163ae732bb942f807d4e3b3198386ccc2a493172
David <david@c940>
2022-08-12 16:21:53 +0200

    change api url

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -6,7 +6,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 const API_ROOT =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : process.env.BACKEND_URL;
+    : "https://anythink-market-jtnr7-api.herokuapp.com/api/ping";
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
