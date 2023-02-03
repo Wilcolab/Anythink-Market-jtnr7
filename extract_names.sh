0e89083dd7192174fbee606cc18b1c1ae8936901
David F <davidfers@protonmail.com>
2023-02-03 16:44:07 +0100

    script to extract the names of @amazon.com users, output to a file named output_names.txt

--- extract_names.sh
+++ extract_names.sh
@@ -15,7 +15,7 @@ IFS=$'\n'
 #Use grep to count the number of lines that contain "amazon.com"
 amazon_lines=($(grep -i "@amazon.com" "$file"))
 for line in "${amazon_lines[@]}"; do
- echo "$line" | awk -F, '{print $3 " "  $2}' >> output.txt
+ echo "$line" | awk -F, '{print $3 " "  $2}' >> output_names.txt
 done
 IFS=$OLDIFS
 
